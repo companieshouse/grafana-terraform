65d7228297ac8beceba6ffb9b6fd325c6195b02f
MartyT <33862629+MartyTaggart@users.noreply.github.com>
2022-04-05 16:16:05 +0100

    Update secgroup and re-add vault info (#6)
    * add version file
    
    * fix sg and add vault info back in for internal cidrs
    
    * concat groups
    
    * block egress
    
    * removing egress and un-necessary ingress rule

--- groups/application/locals.tf
+++ groups/application/locals.tf
@@ -1,8 +1,9 @@
 locals {
 
+  admin_cidrs               = values(data.vault_generic_secret.internal_cidrs.data)
   application_subnet_cidrs  = [for s in data.aws_subnet.application : s.cidr_block]
-  elb_access_logs_prefix      = "elb-access-logs"
-  internal_fqdn = format("%s.%s.aws.internal", split("-", var.aws_account)[1], split("-", var.aws_account)[0])
+  elb_access_logs_prefix    = "elb-access-logs"
+  internal_fqdn             = format("%s.%s.aws.internal", split("-", var.aws_account)[1], split("-", var.aws_account)[0])
 
   default_tags = {
     Terraform   = "true"
