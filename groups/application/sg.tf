eb1d71d6f71be1ccbf21f355a36dce5e0df1cf13
MartyT <martytaggart@gmail.com>
2022-04-06 08:58:40 +0100

    internal cidr fix

--- groups/application/sg.tf
+++ groups/application/sg.tf
@@ -5,18 +5,14 @@ module "gfn_app_ec2_security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 3.0"
 
-  name        = "sgr-${var.application}-app-001"
-  description = "Security group for the ${var.application} app ec2"
-  vpc_id      = data.aws_vpc.vpc.id
+  name                = "sgr-${var.application}-app-001"
+  description         = "Security group for the ${var.application} app ec2"
+  vpc_id              = data.aws_vpc.vpc.id
+  ingress_cidr_blocks = local.internal_cidrs
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
 
-  ingress_with_self = [
-    {
-      rule = "http-80-tcp"
-      self = true
-    }
-  ]
 
-  #egress_rules = ["all-all"]
+#  #egress_rules = ["all-all"]
 }
 
 resource "aws_security_group_rule" "weblogic" {
@@ -39,5 +35,5 @@ resource "aws_security_group_rule" "clients" {
   from_port         = each.value
   to_port           = each.value
   protocol          = "tcp"
-  cidr_blocks       = concat(local.admin_cidrs, var.logstash_client_ips)
+  cidr_blocks       = concat(local.internal_cidrs, var.logstash_client_ips)
 }
