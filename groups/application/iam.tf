5ca2e6bab94ba83bb518d44d40f014fb6efc13fd
MartyT <martytaggart@gmail.com>
2022-04-07 17:59:27 +0100

    update iam to allow r53 script to run

--- groups/application/iam.tf
+++ groups/application/iam.tf
@@ -22,24 +22,16 @@ module "gfn_app_profile" {
     local.ssm_kms_key_id
   ]
   s3_buckets_write = [local.session_manager_bucket_name]
-/*
+
   custom_statements = [
     {
-      sid    = "AllowAccessToReleaseBucket",
-      effect = "Allow",
-      resources = [
-        "arn:aws:s3:::shared-services.eu-west-2.releases.ch.gov.uk/*",
-        "arn:aws:s3:::shared-services.eu-west-2.releases.ch.gov.uk",
-        "arn:aws:s3:::shared-services.eu-west-2.configs.ch.gov.uk/*",
-        "arn:aws:s3:::shared-services.eu-west-2.configs.ch.gov.uk",
-        "arn:aws:s3:::shared-services.eu-west-2.resources.ch.gov.uk/*",
-        "arn:aws:s3:::shared-services.eu-west-2.resources.ch.gov.uk"
-      ],
+      sid       = "AllowWriteToRoute53",
+      effect    = "Allow",
+      resources = ["arn:aws:route53:::hostedzone/${data.aws_route53_zone.private_zone.zone_id}"],
       actions = [
-        "s3:Get*",
-        "s3:List*",
+        "route53:ChangeResourceRecordSets"
       ]
     }
   ]
-*/
+
 }
